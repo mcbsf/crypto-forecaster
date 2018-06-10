2190ebd7c07f41d8af434488d0db1bf6481fe9a6
Leonardo Jose  Schettini de Arruda <leoschettini2@gmail.com>
2018-06-10 14:18:26 -0300

    Update granger main script

--- forecaster/__main__.py
+++ forecaster/__main__.py
@@ -8,39 +8,39 @@ from statsmodels.tsa.stattools import coint
 
 
 df = retriever.get_data('bitcoin')
-price_header = 'open'
+price_header = 'price'
 
-start_date = pd.to_datetime('2013-12-01')
-end_date = pd.to_datetime('2016-02-01')
+start_date = pd.to_datetime('2013-12-02')
+end_date = pd.to_datetime('2014-01-30')
 
 mask = (df['date'] > start_date) & (df['date'] <= end_date)
 
-# df[price_header] = stats.zscore(df[price_header])
-# df['positive_reply'] = stats.zscore(df['positive_reply'])
-# df['very_negative_reply'] = stats.zscore(df['very_negative_reply'])
+df[price_header] = stats.zscore(df[price_header])
+df['positive_reply'] = stats.zscore(df['positive_reply'])
+df['very_negative_reply'] = stats.zscore(df['very_negative_reply'])
 
-# df[price_header] = timeseries.standardize_laggedly(df[price_header])
-# df['positive_reply'] = timeseries.standardize_laggedly(df['positive_reply'])
-# df['very_negative_reply'] = timeseries.standardize_laggedly(df['very_negative_reply'])
+df[price_header] = timeseries.standardize_laggedly(df[price_header])
+df['positive_reply'] = timeseries.standardize_laggedly(df['positive_reply'])
+df['very_negative_reply'] = timeseries.standardize_laggedly(df['very_negative_reply'])
 
-df[price_header] = timeseries.first_differentiate(df[price_header])
-df['positive_reply'] = timeseries.first_differentiate(df['positive_reply'])
-df['very_negative_reply'] = timeseries.first_differentiate(df['very_negative_reply'])
+# df[price_header] = timeseries.first_differentiate(df[price_header])
+# df['positive_reply'] = timeseries.first_differentiate(df['positive_reply'])
+# df['very_negative_reply'] = timeseries.first_differentiate(df['very_negative_reply'])
 
 # Drop nan values
 df = df.dropna()
 
-# Metrics and plots
-timeseries.metrics(df[price_header])
-timeseries.plot(df['date'], df[price_header], '', '')
-
-timeseries.metrics(df['positive_reply'])
-timeseries.plot(df['date'], df['positive_reply'], '', '')
-
-timeseries.metrics(df['very_negative_reply'])
-timeseries.plot(df['date'], df['very_negative_reply'], '', '')
-
 restricted_df = df.loc[mask]
+
+# Metrics and plots
+timeseries.metrics(restricted_df[price_header])
+timeseries.plot(restricted_df['date'], restricted_df[price_header], '', '')
+
+timeseries.metrics(restricted_df['positive_reply'])
+timeseries.plot(restricted_df['date'], restricted_df['positive_reply'], '', '')
+
+timeseries.metrics(restricted_df['very_negative_reply'])
+timeseries.plot(restricted_df['date'], restricted_df['very_negative_reply'], '', '')
 
 # Standalone Granger causality
 stack = np.column_stack((restricted_df[price_header], restricted_df['positive_reply']))
